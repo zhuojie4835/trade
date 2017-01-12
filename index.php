dd6353e45ae1e6bcb42cb638ed05cc7131ce30c0
zhuojie4835 <zhuojie4835@163.com>
2017-01-12 19:47:30 +0800

    前台跳转

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 // +----------------------------------------------------------------------
 // | Author: 麦当苗儿 <zuojiazi@vip.qq.com> <http://www.zjzit.cn>
 // +----------------------------------------------------------------------
+header('Location: ./admin.php');
 
 if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 
