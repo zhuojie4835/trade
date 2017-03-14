299ef77d08625a0fc10105c168836b8982584b62
zhuojie4835 <zhuojie4835@163.com>
2017-03-14 23:34:14 +0800

    整合支付宝

---
+++ Application/Customer/Controller/AlipayController.class.php
@@ -0,0 +1,41 @@
+<?php 
+namespace Customer\Controller;
+use Think\Controller;
+
+class AlipayController extends Controller {
+	public function index() {
+		if(IS_POST) {
+			vendor('Alipay.wappay.service.AlipayTradeService');
+			vendor('Alipay.wappay.buildermodel.AlipayTradeWapPayContentBuilder');
+			$out_trade_no = $_POST['WIDout_trade_no'];
+
+		    //订单名称，必填
+		    $subject = $_POST['WIDsubject'];
+
+		    //付款金额，必填
+		    $total_amount = $_POST['WIDtotal_amount'];
+
+		    //商品描述，可空
+		    $body = $_POST['WIDbody'];
+
+		    //超时时间
+		    $timeout_express="1m";
+
+		    $payRequestBuilder = new \AlipayTradeWapPayContentBuilder();
+		    $payRequestBuilder->setBody($body);
+		    $payRequestBuilder->setSubject($subject);
+		    $payRequestBuilder->setOutTradeNo($out_trade_no);
+		    $payRequestBuilder->setTotalAmount($total_amount);
+		    $payRequestBuilder->setTimeExpress($timeout_express);
+
+		    $config = C('ALIPAY')
+		    $payResponse = new \AlipayTradeService($config);
+		    $result=$payResponse->wapPay($payRequestBuilder,$config['return_url'],$config['notify_url']);
+
+		    return ;
+			var_dump(C('ALIPAY'));
+		}
+
+		$this->display();
+	}
+}
---
+++ ThinkPHP/Library/Vendor/AliPay/config.php
@@ -0,0 +1,28 @@
+<?php
+$config = array (	
+	//应用ID,您的APPID。
+	'app_id' => "2016080400164364",
+
+	//商户私钥，您的原始格式RSA私钥
+	'merchant_private_key' => "MIIEowIBAAKCAQEAyxY+EsKeA9i5LL66avZdye94gBlMib5CAX8iCYPfKey3WA2IN9QiXlTentLUx55y7zHI/qQomShK3otjl6ri+Sfo71X4Z0iKmatORhQoYOwPC2BBwctAMo2YEE/sSid1uYnkE+tVmzNlfQXHwQYn9EWbbiYUPmb765UIZk5ciFFFYbRM0OV4WPXH4FYgLR1fjFcxYOHa8tGzrSF2gV6TeFGoRuYosuXyJIXQAarwrDqY/kuQmFf8d0SpzPpmLR+rhaUms828ZSjI/hVQFGZ5bkTVl1m5Y7UXI3ZmWlEyWs2Dc7GE4qDxmgBZcVIGgt9kS+1vT1z3C24dvpl32Z4a4QIDAQABAoIBAQCAuYR216zYu1IELpByo94m1QcICwEcfd/Qmwi0B0Y4iLZdtZYV7Pwr1peVDAWa0bAANQo1fU/OZF+wV6G0zLg5PTbEHTXqIWzYomBmwvglFvsiNsz3TSFP6bfs/vvCtOhFxkUu6wfD6/v0FunaS1Cf4E74rmI+e4BKfphoYFr+e4MZDPxd5Idm+RSEo5anX9J/dj8F1DB4EeaBCHScrUgcf6dt6+UULkPX6M3eHoRIMxehSYkkfQEebROqm8S2BPrXiFA8alqGFiWCBhF64fHteoBhCmDVx0bi3TBp3YsdkKTMsrkLS5Rr6ETnUMUONdyxTl7Y1CzE8hz4PXtnBzQBAoGBAPZK5fi+qnAjSuNa4o92lKbZ96mNthwG+GEkm30XaVUTKxnLqcg53jcolRThVk9gXdrkoUXWz4FFfgMftCZkpttHXd7ZWrMVVlzdQkFO9URTRkBXRZpFnoZAverxZxGG0tDiukZqr12j9xp9p4BhZCgzfeGvybW3y5YNuDEMevJRAoGBANMXZabbIVZ6vIu+H48qmZ7XpAt8Lr0xsfBhPHR9Mt3AobaN2fn5RLd4ZiHnFrd9drzwd19zRx8mvlSwlMQ/CFNusrX9PZM02YY7hiZbOZC2PlGitvQ/oyu6ySA7WPo67E+7fdogdCGPKdqmU2e+EiFBde/WbfkeAFb3V4eBw2uRAoGALFZzbAI3AJT56E/2NUltnVPj59whCo6erC5A55YNWklnGu+1EtyICnn9zJJ8TTHV84/xEHeJR4ZRFxLgBYFdIGCKn5GVaGPQ9krGoKcsNC6hmPedCha6YQTKq77lhRw2W3BVhFO0WjEoTNaODuh5dVs/sB9LOrFprwutOvc6MuECgYBvPfBC6035TfsYxZvKDdAhJlD8AHh9GbhFDmkWlnf85qwSEVUi59rvuRJRoX+WORce4LZ35b9ASmGZLeloNjOEDF0/jPIrJnnPxISgqAiBG5lh2hcCRpsNy4R8aOouDGlGZ28eSWYJ3XFYGbjwffj974pgVydjVg0lx3koHxlGEQKBgDqCevaz8XJxpZt7QyYX7LJ6+OxfB8WQyZVQkrWl2pfq/EKdyaI2cWfopBXP2dEI06jZXmlLYWe6berYbTNU+bRkjBFKLx2ljaXydsk4xfhIwOpsh0xhBHYqynnfN51LT4C9Nqi8Lj604JfDxyqdEVsRBFgDF6jDpGrED2VfVTSR",
+	
+	//异步通知地址
+	'notify_url' => "http://test.local/alipay.trade.wap.pay-PHP-UTF-8/notify_url.php",
+	
+	//同步跳转
+	'return_url' => "http://mitsein.com/alipay.trade.wap.pay-PHP-UTF-8/return_url.php",
+
+	//编码格式
+	'charset' => "UTF-8",
+
+	//签名方式
+	'sign_type'=>"RSA2",
+
+	//支付宝网关
+	'gatewayUrl' => "https://openapi.alipay.com/gateway.do",
+
+	//支付宝公钥,查看地址：https://openhome.alipay.com/platform/keyManage.htm 对应APPID下的支付宝公钥。
+	'alipay_public_key' => "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAyxY+EsKeA9i5LL66avZdye94gBlMib5CAX8iCYPfKey3WA2IN9QiXlTentLUx55y7zHI/qQomShK3otjl6ri+Sfo71X4Z0iKmatORhQoYOwPC2BBwctAMo2YEE/sSid1uYnkE+tVmzNlfQXHwQYn9EWbbiYUPmb765UIZk5ciFFFYbRM0OV4WPXH4FYgLR1fjFcxYOHa8tGzrSF2gV6TeFGoRuYosuXyJIXQAarwrDqY/kuQmFf8d0SpzPpmLR+rhaUms828ZSjI/hVQFGZ5bkTVl1m5Y7UXI3ZmWlEyWs2Dc7GE4qDxmgBZcVIGgt9kS+1vT1z3C24dvpl32Z4a4QIDAQAB",
+		
+	
+);
