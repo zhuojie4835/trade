0d87fafc1ec189f5f6301b8ba418069953f3b5c9
zhuojie4835 <zhuojie4835@163.com>
2017-03-15 10:12:47 +0800

    alipay_pc_md5

--- Application/Customer/Controller/AlipayController.class.php
+++ Application/Customer/Controller/AlipayController.class.php
@@ -3,6 +3,9 @@ namespace Customer\Controller;
 use Think\Controller;
 
 class AlipayController extends Controller {
+	/*
+	手机网页支付
+	 */
 	public function index() {
 		if(IS_POST) {
 			vendor('AliPay.wappay.service.AlipayTradeService');
@@ -37,6 +40,55 @@ class AlipayController extends Controller {
 
 		    return ;
 			var_dump(C('ALIPAY'));
+		}
+
+		$this->display();
+	}
+
+	/*
+	pc网页支付
+	 */
+	public function pc() {
+		if(IS_POST) {
+			// vendor("AliPay.pcpay_md5.alipay_config");
+			require_once (VENDOR_PATH."AliPay/pcpay_md5/alipay_config.php");
+			vendor("AliPay.pcpay_md5.lib.alipay_submit");
+			
+			/**************************请求参数**************************/
+	        //商户订单号，商户网站订单系统中唯一订单号，必填
+	        $out_trade_no = $_POST['WIDout_trade_no'];
+	        //订单名称，必填
+	        $subject = $_POST['WIDsubject'];
+	        //付款金额，必填
+	        $total_fee = $_POST['WIDtotal_fee'];
+	        //商品描述，可空
+	        $body = $_POST['WIDbody'];
+
+			/************************************************************/
+			//构造要请求的参数数组，无需改动
+			$parameter = array(
+				"service"       => $alipay_config['service'],
+				"partner"       => $alipay_config['partner'],
+				"seller_id"  => $alipay_config['seller_id'],
+				"payment_type"	=> $alipay_config['payment_type'],
+				"notify_url"	=> $alipay_config['notify_url'],
+				"return_url"	=> $alipay_config['return_url'],
+				
+				"anti_phishing_key"=>$alipay_config['anti_phishing_key'],
+				"exter_invoke_ip"=>$alipay_config['exter_invoke_ip'],
+				"out_trade_no"	=> $out_trade_no,
+				"subject"	=> $subject,
+				"total_fee"	=> $total_fee,
+				"body"	=> $body,
+				"_input_charset"	=> trim(strtolower($alipay_config['input_charset']))
+				//其他业务参数根据在线开发文档，添加参数.文档地址:https://doc.open.alipay.com/doc2/detail.htm?spm=a219a.7629140.0.0.kiX33I&treeId=62&articleId=103740&docType=1
+		        //如"参数名"=>"参数值"	
+			);
+
+			//建立请求
+			$alipaySubmit = new \AlipaySubmit($alipay_config);
+			$html_text = $alipaySubmit->buildRequestForm($parameter,"get", "确认");
+			echo $html_text;
 		}
 
 		$this->display();
